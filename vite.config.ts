766ab3e6b706839ac35ddcb0b8b535628c3f6b5b
سینا خسروی <serverenglis063@gmail.com>
2026-02-20 08:04:13 +0330

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+ base: '/nimble-tunnel-go/',
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import { VitePWA } from "vite-plugin-pwa";
